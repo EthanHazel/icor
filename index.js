b1e04ee3af5df4fe8d296e2c25dfe39684907148
Ethan Hazel <ethanyvonhazel@gmail.com>
2025-05-20 21:28:50 -0400

    update(compileIco): allowing 512

--- index.js
+++ index.js
@@ -21,8 +21,14 @@ function compileIco(images) {
   images.forEach((img, index) => {
     const offset = headerSize + index * directoryEntrySize;
 
-    finalBuffer.writeUInt8(img.width === 256 ? 0 : img.width, offset);
-    finalBuffer.writeUInt8(img.height === 256 ? 0 : img.height, offset + 1);
+    finalBuffer.writeUInt8(
+      img.width === 256 || img.width === 512 ? 0 : img.width,
+      offset
+    );
+    finalBuffer.writeUInt8(
+      img.height === 256 || img.height === 512 ? 0 : img.height,
+      offset + 1
+    );
     finalBuffer.writeUInt8(0, offset + 2); // Color palette
     finalBuffer.writeUInt8(0, offset + 3); // Reserved
     finalBuffer.writeUInt16LE(1, offset + 4); // Color planes
